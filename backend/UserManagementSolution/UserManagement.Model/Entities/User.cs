98516037899c135f62bfc9020efda61c099c92bb
MohammadJavad <mohammadjs2642@gmail.com>
2025-11-14 22:56:53 +0330

    user management project init

---
+++ backend/UserManagementSolution/UserManagement.Model/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿namespace UserManagement.Domain.Entities;
+
+public class User
+{
+    public User(string username, Email email, string passwordHash)
+    {
+        Username = username;
+        Email = email;
+        PasswordHash = passwordHash;
+        IsActive = true;
+    }
+
+    public int Id { get; private set; }
+    public string Username { get; private set; }
+    public Email Email { get; private set; }
+    public string PasswordHash { get; private set; }
+    public bool IsActive { get; private set; }
+
+}
