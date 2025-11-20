a5322b8f6d3e6eac3d3dea1416963a733750676f
MohammadJavad <mohammadjs2642@gmail.com>
2025-11-20 12:30:23 +0330

    folder structure changed also unit test and integration test added to solution

---
+++ backend/UserManagementSolution/tests/UserManagement.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace UserManagement.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ backend/UserManagementSolution/tests/UserManagement.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace UserManagement.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
