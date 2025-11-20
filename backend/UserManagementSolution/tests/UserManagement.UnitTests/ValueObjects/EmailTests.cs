6e632d6045562bb695f86e4f682d79f9adedca1e
MohammadJavad <mohammadjs2642@gmail.com>
2025-11-20 13:21:29 +0330

    som unit tests  for the Email value object added

---
+++ backend/UserManagementSolution/tests/UserManagement.UnitTests/ValueObjects/EmailTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using UserManagement.Domain.ValueObjects;
+
+namespace UserManagement.UnitTests.ValueObjects;
+
+public class EmailTests
+{
+    [Theory]
+    [InlineData("user@gmail.com")]
+    [InlineData("text@example.com")]
+    public void CreateEmail_WithValidValue_ShouldSetValue(string emailString)
+    {
+        // Act
+        var email = new Email(emailString);
+        // Assert
+        email.Value.Should().Be(emailString);
+        email.ToString().Should().Be(emailString);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("       ")]
+    [InlineData(null)]
+    [InlineData("invalid.com")]
+    public void CreateEmail_WithInvalidValue_ShouldThrowArgumentException(string emailString)
+    {
+        // Act
+        var email = () => new Email(emailString!);
+        // Assert
+        email.Should().Throw<ArgumentException>().WithMessage("*Invalid email address*");
+    }
+
+    [Fact]
+    public void Email_ShouldBe_Immmutable()
+    {
+        var email = new Email("test@gmail.com");
+        typeof(Email).GetProperty(nameof(email.Value))!.CanWrite.Should().BeFalse();
+    }
+}
