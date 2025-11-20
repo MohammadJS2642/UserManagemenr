9a4d0fce18daf0c765dbb6f01b980b8684210c77
MohammadJavad <mohammadjs2642@gmail.com>
2025-11-20 22:04:13 +0330

    user ctor changed

--- frontend/src/app/user/user.ts
+++ frontend/src/app/user/user.ts
@@ -1,19 +1,32 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { UserService } from './infrastructure/user-service';
 import { UserModel } from './domain/user.model';
 
 @Component({
   selector: 'app-user',
+  standalone: true,
   imports: [],
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
 export class User {
   private readonly userService = inject(UserService);
-  users: UserModel[] | null = [];
+  users = signal<UserModel[] | null>([]);
+
+  /**
+   *
+   */
+  constructor() {
+    effect(() => {
+      this.userService.getAll().subscribe({
+        next: (res) => this.users.set(res),
+        error: (err) => console.error(err),
+      });
+    });
+  }
 
   loadUsers() {
-    this.userService.getAll().subscribe((res) => (this.users = res));
+    this.userService.getAll().subscribe((res) => this.users.set(res));
   }
 
   remove(id: number) {
