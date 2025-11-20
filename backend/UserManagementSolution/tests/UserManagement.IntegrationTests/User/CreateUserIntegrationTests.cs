04806f994853cc1e2be9e5af994c49e689c8f885
MohammadJavad <mohammadjs2642@gmail.com>
2025-11-20 13:35:58 +0330

    create user integration test added

---
+++ backend/UserManagementSolution/tests/UserManagement.IntegrationTests/User/CreateUserIntegrationTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using UserManagement.Application.UseCases.User;
+using UserManagement.Domain.Entities;
+using UserManagement.Infrastructure.Persistence;
+
+namespace UserManagement.IntegrationTests.User;
+
+public class CreateUserIntegrationTests
+{
+    private readonly DbContextOptions<UserManagementDbContext> _contextOptions;
+    public CreateUserIntegrationTests()
+    {
+        _contextOptions = new DbContextOptionsBuilder<UserManagementDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+    }
+
+    [Fact]
+    public async Task Handle_Should_CreateUser_InDatabase()
+    {
+        using var context = new UserManagementDbContext(_contextOptions);
+        var repository = new UserRepository(context);
+        var uow = new UnitOfWork(context);
+
+        var handle = new CreateUserUseCase(uow, repository);
+
+        var command = await handle.ExecuteAsync("testuser", "email@email.com", "password");
+
+        var userFromDb = await repository.GetByIdAsync(command.Id);
+
+        userFromDb.Should().NotBeNull();
+        userFromDb!.Username.Should().Be("testuser");
+    }
+}
