537a81694b1df95224f41788130fcee624cdc5e1
MohammadJavad <mohammadjs2642@gmail.com>
2025-11-21 22:19:35 +0330

    header, sidebar and routing added to angular project with simple ui

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router';
 import { User } from './user/user';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-export const routes: Routes = [{ path: '', component: User }];
+export const routes: Routes = [
+  { path: '', component: Dashboard },
+  { path: 'users', component: User },
+];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Sidebar } from './components/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Sidebar, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('frontend');
