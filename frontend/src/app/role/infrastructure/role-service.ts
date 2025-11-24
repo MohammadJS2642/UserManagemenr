f309b057f3706e1d73cb9b466c450142cbf98fec
MohammadJavad <mohammadjs2642@gmail.com>
2025-11-24 11:37:21 +0330

    role model and create role added to front

--- frontend/src/app/role/infrastructure/role-service.ts
+++ frontend/src/app/role/infrastructure/role-service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { RoleModel } from '../domain/Role.Model';
+import { CreateRole } from '../domain/create-role.model';
 
 @Injectable({ providedIn: 'root' })
 export class RolseService {
@@ -10,5 +11,9 @@ export class RolseService {
 
   getAll(): Observable<RoleModel[]> {
     return this.http.get<RoleModel[]>(`${this.baseUrl}/GetRoles`);
+  }
+
+  createUser(request: CreateRole): Observable<RoleModel> {
+    return this.http.post<CreateRole>(`${this.baseUrl}`, request);
   }
 }
--- frontend/src/app/role/role.ts
+++ frontend/src/app/role/role.ts
@@ -1,18 +1,22 @@
 import { Component, inject, signal } from '@angular/core';
 import { RolseService } from './infrastructure/role-service';
 import { RoleModel } from './domain/Role.Model';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CreateRole } from './domain/create-role.model';
 
 @Component({
   selector: 'app-role',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './role.html',
   styleUrl: './role.css',
 })
 export class Role {
   private readonly roleService = inject(RolseService);
   roles = signal<RoleModel[] | null>([]);
-
+  requestForm = new FormGroup({
+    name: new FormControl(''),
+  });
   /**
    *
    */
@@ -24,6 +28,17 @@ export class Role {
     this.roleService.getAll().subscribe({
       next: (res) => this.roles.set(res),
       error: (err) => console.log(err),
+    });
+  }
+
+  createRole() {
+    const request: CreateRole = this.requestForm.value as CreateRole;
+    this.roleService.createUser(request).subscribe({
+      next: (res) => {
+        this.requestForm.reset();
+        this.getAll();
+      },
+      error: (err) => console.log('create role error: ', err),
     });
   }
 }
